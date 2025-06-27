3e005556a3b4d002fba780fbbfacdde85872ca9b
Robert Leist <robert.leist@dfki.de>
2025-06-27 13:00:16 +0200

    refactor: completely refactored api to be more modular and easier to update.

--- src/api/masks.js
+++ src/api/masks.js
@@ -1,3 +1,5 @@
+import { handleApiError } from "../api/util";
+
 const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || "https://coral.ni.dfki.de/api";
 
--- src/api/segmentation.js
+++ src/api/segmentation.js
@@ -1,3 +1,4 @@
+import { handleApiError } from "../api/util";
 
 const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || "https://coral.ni.dfki.de/api";
