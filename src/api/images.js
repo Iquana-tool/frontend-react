53c579629350923431dcddd3a2c7b700cec5238e
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2025-07-30 15:05:35 +0200

    fix: remove extra space in API base URL definition in images.js

--- src/api/images.js
+++ src/api/images.js
@@ -1,7 +1,7 @@
 import { handleApiError } from "../api/util";
 
 const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || " http://127.0.0.1:8000";
+    process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 
 // Fetch list of available images
 export const fetchImages = async (datasetId) => {
