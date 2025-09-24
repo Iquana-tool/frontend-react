0ec9f317a1c57b2f21176bc5a2669fe687843a56
prathmesh3235 <prathmesh.admg@gmail.com>
2025-09-24 14:57:20 +0200

    Implemented AnnotationPageV2 component with MainLayout and ResponsiveWrapper for improved layout structure

---
+++ src/pages/AnnotationPageV2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from '../components/annotationPage/layout/MainLayout';
+import ResponsiveWrapper from '../components/annotationPage/layout/ResponsiveWrapper';
+
+const AnnotationPageV2 = () => {
+  return (
+    <ResponsiveWrapper>
+      <MainLayout />
+    </ResponsiveWrapper>
+  );
+};
+
+export default AnnotationPageV2;
