9c1b7bd7c9bd3fda70eb88d86abd6f655148680a
prathmesh3235 <prathmesh.admg@gmail.com>
2025-09-24 15:40:23 +0200

    Add annotation selectors for centralized state management, including tools, canvas, segmentation, images, models, and objects. Implemented actions for managing current tool, prompts, segmentation, and object selection to enhance annotation capabilities.

---
+++ src/stores/selectors/annotationSelectors.js
@@ -0,0 +1,59 @@
+import useAnnotationStore from '../useAnnotationStore';
+import { shallow } from 'zustand/shallow';
+
+// Simple state selectors 
+export const useCurrentTool = () => useAnnotationStore(state => state.ui.currentTool);
+
+// Canvas selectors
+export const useCanvasPrompt = () => useAnnotationStore(state => state.canvas.prompt);
+export const useIsPrompting = () => useAnnotationStore(state => state.canvas.isPrompting);
+
+// Segmentation selectors
+export const useCurrentMask = () => useAnnotationStore(state => state.segmentation.currentMask);
+
+export const useCurrentImage = () => useAnnotationStore(state => state.images.currentImage);
+export const useCurrentImageId = () => useAnnotationStore(state => state.images.currentImageId);
+export const useImageList = () => useAnnotationStore(state => state.images.imageList);
+export const useAnnotationStatus = () => useAnnotationStore(state => state.images.annotationStatus);
+
+export const useSelectedModel = () => useAnnotationStore(state => state.models.selectedModel);
+export const useCompletionModel = () => useAnnotationStore(state => state.models.completionModel);
+
+export const useObjectsList = () => useAnnotationStore(state => state.objects.list);
+export const useSelectedObjects = () => useAnnotationStore(state => state.objects.selected);
+export const useObjectsVisibility = () => useAnnotationStore(
+  state => state.objects.visibility,
+  (a, b) => 
+    a.showAll === b.showAll && 
+    a.rootLevelOnly === b.rootLevelOnly && 
+    a.selectedLevelOnly === b.selectedLevelOnly &&
+    JSON.stringify(a.labels) === JSON.stringify(b.labels)
+);
+export const useObjectColors = () => useAnnotationStore(state => state.objects.colors);
+
+// Action selectors
+export const useSetCurrentTool = () => useAnnotationStore(state => state.setCurrentTool);
+
+// Canvas actions
+export const useSetPrompt = () => useAnnotationStore(state => state.setPrompt);
+export const useSetIsPrompting = () => useAnnotationStore(state => state.setIsPrompting);
+
+// Segmentation actions
+export const useStartSegmentation = () => useAnnotationStore(state => state.startSegmentation);
+export const useSetCurrentMask = () => useAnnotationStore(state => state.setCurrentMask);
+
+// Object actions
+export const useAddObject = () => useAnnotationStore(state => state.addObject);
+export const useRemoveObject = () => useAnnotationStore(state => state.removeObject);
+export const useSelectObject = () => useAnnotationStore(state => state.selectObject);
+export const useDeselectObject = () => useAnnotationStore(state => state.deselectObject);
+export const useClearSelection = () => useAnnotationStore(state => state.clearSelection);
+
+export const useSetSelectedModel = () => useAnnotationStore(state => state.setSelectedModel);
+export const useSetCompletionModel = () => useAnnotationStore(state => state.setCompletionModel);
+export const useSetVisibilityMode = () => useAnnotationStore(state => state.setVisibilityMode);
+export const useToggleVisibility = () => useAnnotationStore(state => state.toggleVisibility);
+export const useSetCurrentImage = () => useAnnotationStore(state => state.setCurrentImage);
+export const useSetImageList = () => useAnnotationStore(state => state.setImageList);
+export const useSetAnnotationStatus = () => useAnnotationStore(state => state.setAnnotationStatus);
+
