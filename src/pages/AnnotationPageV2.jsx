f06737c4bdaaef67814dd11d093d5c38f085e76a
prathmesh3235 <prathmesh.admg@gmail.com>
2025-09-25 10:28:20 +0200

    Refactored AnnotationPageV2 to include DatasetLoader and DatasetNavigation components, enhancing layout structure and user experience by providing better dataset management and navigation options.

--- src/pages/AnnotationPageV2.jsx
+++ src/pages/AnnotationPageV2.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import MainLayout from '../components/annotationPage/layout/MainLayout';
 import ResponsiveWrapper from '../components/annotationPage/layout/ResponsiveWrapper';
+import DatasetLoader from '../components/annotationPage/layout/DatasetLoader';
+import DatasetNavigation from '../components/annotationPage/layout/DatasetNavigation';
 
 const AnnotationPageV2 = () => {
   return (
-    <ResponsiveWrapper>
-      <MainLayout />
-    </ResponsiveWrapper>
+    <DatasetLoader>
+      <ResponsiveWrapper>
+        <div className="min-h-screen bg-gray-50">
+          <DatasetNavigation />
+          <MainLayout />
+        </div>
+      </ResponsiveWrapper>
+    </DatasetLoader>
   );
 };
 
